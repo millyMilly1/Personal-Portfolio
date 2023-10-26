2785b4365052e3ef7ef93bb2183bef414c5c13ba
millyMilly1 <chukwurahenry@gmail.com>
2023-10-26 17:34:35 +0100

    experience icons and skill update

--- personal-portfolio/src/components/experience/experience.jsx
+++ personal-portfolio/src/components/experience/experience.jsx
@@ -14,29 +14,44 @@ const experience = () => {
           <h3>Web development</h3>
           <div className='experience__content'>
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
+              <BsFillPatchCheckFill className='experience__details-icon'  />
+              <div>
               <h4>HTML</h4>
               <small className='text-light'>Experience</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>CSS</h4>
               <small className='text-light'>Intermidiate</small>
+              </div>
+
             </article>
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>JavaScript</h4>
               <small className='text-light'>Experience</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>React</h4>
               <small className='text-light'>Experience</small>
+              </div>
+              
             </article>
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>PHP</h4>
               <small className='text-light'>Experience</small>
+              </div>
+              
             </article>
           </div>
           </div>
@@ -44,30 +59,45 @@ const experience = () => {
           <div className='experience__backend'>
           <h3>Networking technologies</h3>
           <div className='experience__content'>
+
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
-              <h4>HTML</h4>
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
+              <h4>Network Topology Planning</h4>
               <small className='text-light'>Experience</small>
+               </div>
             </article>
+
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
-              <h4>CSS</h4>
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
+              <h4>Network Design</h4>
               <small className='text-light'>Intermidiate</small>
+              </div>
             </article>
+
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
-              <h4>JavaScript</h4>
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div> 
+              <h4>Router Configuration</h4>
               <small className='text-light'>Experience</small>
+              </div>
             </article>
+
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
-              <h4>React</h4>
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
+              <h4>Subnetting</h4>
               <small className='text-light'>Experience</small>
+              </div>
             </article>
+
             <article className='experience__details'>
-              <BsFillPatchCheckFill />
-              <h4>Tailwind</h4>
+              <BsFillPatchCheckFill className='experience__details-icon' />
+              <div>
+              <h4>Firewall Configuration</h4>
               <small className='text-light'>Experience</small>
+              </div>
             </article>
           </div>
           </div>
