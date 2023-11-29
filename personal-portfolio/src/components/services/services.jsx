4994c41d8c9a11bc65f1479e6c53ad3dc0f30997
millyMilly1 <chukwurahenry@gmail.com>
2023-11-29 23:21:58 +0100

    new service offered section

--- personal-portfolio/src/components/services/services.jsx
+++ personal-portfolio/src/components/services/services.jsx
@@ -124,6 +124,44 @@ const services = () => {
             </ul>
           </article>
            {/* end of third service*/}
+
+           <article className='services'>
+            <div className='services__head'>
+              <h3>web development</h3>
+            </div>
+
+            <ul className='services__list'>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+              <li>
+                <BiCheck className="service__list-icon" />
+                <p>consectetur adipiscing elit. Nulla facilisi.</p>
+              </li>
+            </ul>
+          </article>
+           {/* end of third service*/}
         </div>
     </section>
   )
