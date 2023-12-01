39aab6f5553115165f35cc1ca875fa8fe183fb01
millyMilly1 <chukwurahenry@gmail.com>
2023-12-01 03:12:33 +0100

    edit on services section

--- personal-portfolio/src/components/services/services.jsx
+++ personal-portfolio/src/components/services/services.jsx
@@ -124,46 +124,6 @@ const services = () => {
             </ul>
           </article>
            {/* end of third service*/}
-
-           <article className='services'>
-            <div className='services__head'>
-              <h3>web development</h3>
-            </div>
-
-            <ul className='services__list'>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-              <li>
-                <BiCheck className="service__list-icon" />
-                <p>consectetur adipiscing elit. Nulla facilisi.</p>
-              </li>
-            </ul>
-            
-          </article>
-           {/* end of third service*/}
-
         </div>
     </section>
   )
